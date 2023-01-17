d1313a3b770e765d9ce51ec543f1400d4f52e3ad
Ismatulla Mansurov <47342870+sapoepsilon@users.noreply.github.com>
2023-01-16 19:45:28 -0700

    fix: ran linter fix

--- src/Izzy/navBar.js
+++ src/Izzy/navBar.js
@@ -1,15 +1,14 @@
-
-const navbar=function() {
+const navbar = function () {
     return {
         isOpen: true,
         entries: [
-            { label:'home', url: '#home', id:1 },
-            { label:'about', url: '#about', id:2 },
-            { label:'skills', url: '#skills', id:3 },
-            { label:'projects', url: '#projects', id:4 },
-            { label:'contact', url: '#contact', id:5 },
+            { label: 'home', url: '#home', id: 1 },
+            { label: 'about', url: '#about', id: 2 },
+            { label: 'skills', url: '#skills', id: 3 },
+            { label: 'projects', url: '#projects', id: 4 },
+            { label: 'contact', url: '#contact', id: 5 },
         ],
     };
-}
+};
 window.navbar = navbar;
 console.log(navbar().open);

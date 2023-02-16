0e5f580d63c58e30aacb7a4ce8571c4c74d965c2
Daniel Teleginski Camargo <daniel.camargo@ensign.edu>
2023-02-15 21:06:30 -0700

    fix: fixing layout

--- src/danielcamargo/components.js
+++ src/danielcamargo/components.js
@@ -43,17 +43,16 @@ const skills = function () {
             HTML: {
                 image: './html5.svg',
                 description:
-                    'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+                    'HTML is essential for creating web content with a wide range of applications, constantly evolving to meet changing needs and remaining vital.',
             },
             CSS: {
                 image: './css.svg',
-                description:
-                    'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+                description: 'CSS is critical for creating visually appealing and consistent web designs across devices and platforms.',
             },
             JavaScript: {
                 image: './js.svg',
                 description:
-                    'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien.',
+                    'JavaScript has grown to become one of the most popular programming languages in the world, with a large and active developer community.',
             },
         },
         async load() {
@@ -61,6 +60,7 @@ const skills = function () {
             const stats = await res.json();
             const { data } = stats;
             const languagesIWant = ['HTML', 'CSS', 'JavaScript'];
+            // const languagesIWant = ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'PHP', 'Kotlin'];
             this.languages = data.languages.filter(l => languagesIWant.indexOf(l.name) !== -1);
         },
         progress(language) {

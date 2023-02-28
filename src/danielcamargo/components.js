55007910ba1cb57412f6bed8a3e19345d3c71cc4
Wesley dos Santos <wesley@Wesleys-MacBook-Air.local>
2023-02-27 19:01:16 -0700

    chore: wip

--- src/danielcamargo/components.js
+++ src/danielcamargo/components.js
@@ -11,6 +11,7 @@ const navbar = function () {
         ],
     };
 };
+window.$navbar = navbar;
 
 const home = function () {
     let $refs;
@@ -33,5 +34,23 @@ const home = function () {
     };
 };
 
-window.$navbar = navbar;
 window.$home = home;
+
+const skills = function () {
+    return {
+        languages: [],
+        async load() {
+            const res = await fetch('https://nextjs-red-six-46.vercel.app/api/wakatime/WesleyDSantos', { method: 'GET' });
+            const stats = await res.json();
+            const { data } = stats;
+            const languagesIWant = ['HTML', 'CSS', 'JavaScript', 'Dart'];
+            this.languages = data.languages.filter(l => languagesIWant.indexOf(l.name) !== -1);
+        },
+        calculatePercentage(language) {
+            const percentage = (language.decimal / 320) * 100;
+            return `${percentage}%`;
+        },
+    };
+};
+
+window.$skills = skills;
